d4fe972a6d6a612e789d0ebea55effedeb6c2286
mdfahimhasan <77580408+mdfahimhasan@users.noreply.github.com>
2024-09-04 12:46:44 -0600

    rearranged code blocks

--- Codes/data_download_preprocess/run_download_preprocess.py
+++ Codes/data_download_preprocess/run_download_preprocess.py
@@ -10,10 +10,7 @@ WestUS_shape = '../../Data_main/shapefiles/Western_US_ref_shapes/WestUS_states.s
 WestUS_raster = '../../Data_main/reference_rasters/Western_US_refraster_2km.tif'
 GEE_merging_refraster_large_grids = '../../Data_main/reference_rasters/GEE_merging_refraster_larger_grids.tif'
 
-# # Data download
-skip_download_gee_data = True
-skip_download_OpenET_data = True
-
+# # # #  data download args # # # #
 gee_data_list = ['Field_capacity', 'Bulk_density', 'Sand_content', 'Clay_content',
                  'GRIDMET_RET', 'GRIDMET_vap_pres_def','GRIDMET_max_RH', 'GRIDMET_Precip',
                  'GRIDMET_min_RH', 'GRIDMET_wind_vel', 'GRIDMET_short_rad', 'DAYMET_sun_hr',
@@ -29,36 +26,18 @@ openET_data_list = ['Irrig_crop_OpenET_IrrMapper',
 
 years = [2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010,
          2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020]
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-# openET 2000-2012 data will be contributed by DRI collaborators
-# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+months = (1, 12)
 
-months = (1, 3)  # then (11, 12) # then openet ensemble monthly from 2013-2020 (1, 3) and (11, 12)
 gee_grid_shape_large = '../../Data_main/shapefiles/Western_US_ref_shapes/WestUS_gee_grid.shp'
 gee_grid_shape_for30m_IrrMapper = '../../Data_main/shapefiles/Western_US_ref_shapes/WestUS_gee_grid_for30m_IrrMapper.shp'
 gee_grid_shape_for30m_LANID = '../../Data_main/shapefiles/Western_US_ref_shapes/WestUS_gee_grid_for30m_LANID.shp'
 data_download_dir = '../../Data_main/Raster_data'
 use_cpu_while_multidownloading = 15
 
-download_all_datasets(year_list=years, month_range=months,
-                      grid_shape_large=gee_grid_shape_large,
-                      data_download_dir=data_download_dir,
-                      gee_data_list=gee_data_list,
-                      skip_download_gee_data=skip_download_gee_data,
-                      use_cpu_while_multidownloading=use_cpu_while_multidownloading)
+skip_download_gee_data = True
+skip_download_OpenET_data = True
 
-download_all_openET_datasets(year_list=years, month_range=months,
-                             grid_shape_for_2km_ensemble=gee_grid_shape_large,
-                             grid_shape_for30m_irrmapper=gee_grid_shape_for30m_IrrMapper,
-                             grid_shape_for30m_lanid=gee_grid_shape_for30m_LANID,
-                             openET_data_list=openET_data_list,
-                             data_download_dir=data_download_dir,
-                             GEE_merging_refraster=GEE_merging_refraster_large_grids,
-                             westUS_refraster=WestUS_raster, westUS_shape=WestUS_shape,
-                             skip_download_OpenET_data=skip_download_OpenET_data,
-                             use_cpu_while_multidownloading=use_cpu_while_multidownloading)
-
-# # Run preprocessing steps
+# # # #  data preprocess args # # # #
 skip_prism_processing = True
 skip_gridmet_precip_processing = True
 skip_gridmet_RET_precessing = True
@@ -77,21 +56,40 @@ skip_process_AWC_data = True
 skip_process_GrowSeason_data = True
 
 
-run_all_preprocessing(skip_prism_processing=skip_prism_processing,
-                      skip_gridmet_precip_processing=skip_gridmet_precip_processing,
-                      skip_gridmet_RET_precessing=skip_gridmet_RET_precessing,
-                      skip_merging_rainfed_frac=skip_merging_rainfed_frac,
-                      skip_merging_rainfed_cropET=skip_merging_rainfed_cropET,
-                      skip_merging_irrigated_frac=skip_merging_irrigated_frac,
-                      skip_merging_irrigated_cropET=skip_merging_irrigated_cropET,
-                      skip_classifying_irrigated_rainfed_cropland=skip_classifying_irrigated_rainfed_cropland,
-                      skip_filtering_irrigated_rainfed_cropET=skip_filtering_irrigated_rainfed_cropET,
-                      skip_summing_irrigated_cropET=skip_summing_irrigated_cropET,
-                      skip_summing_rainfed_cropET=skip_summing_rainfed_cropET,
-                      skip_openET_processing=skip_openET_processing,
-                      skip_excess_ET_filter_processing=skip_excess_ET_filter_processing,
-                      skip_processing_slope_data=skip_processing_slope_data,
-                      skip_process_AWC_data=skip_process_AWC_data,
-                      skip_process_GrowSeason_data=skip_process_GrowSeason_data,
-                      ref_raster=WestUS_raster)
+if __name__ == '__main__':
+    download_all_datasets(year_list=years, month_range=months,
+                          grid_shape_large=gee_grid_shape_large,
+                          data_download_dir=data_download_dir,
+                          gee_data_list=gee_data_list,
+                          skip_download_gee_data=skip_download_gee_data,
+                          use_cpu_while_multidownloading=use_cpu_while_multidownloading)
+
+    download_all_openET_datasets(year_list=years, month_range=months,
+                                 grid_shape_for_2km_ensemble=gee_grid_shape_large,
+                                 grid_shape_for30m_irrmapper=gee_grid_shape_for30m_IrrMapper,
+                                 grid_shape_for30m_lanid=gee_grid_shape_for30m_LANID,
+                                 openET_data_list=openET_data_list,
+                                 data_download_dir=data_download_dir,
+                                 GEE_merging_refraster=GEE_merging_refraster_large_grids,
+                                 westUS_refraster=WestUS_raster, westUS_shape=WestUS_shape,
+                                 skip_download_OpenET_data=skip_download_OpenET_data,
+                                 use_cpu_while_multidownloading=use_cpu_while_multidownloading)
+
+    run_all_preprocessing(skip_prism_processing=skip_prism_processing,
+                          skip_gridmet_precip_processing=skip_gridmet_precip_processing,
+                          skip_gridmet_RET_precessing=skip_gridmet_RET_precessing,
+                          skip_merging_rainfed_frac=skip_merging_rainfed_frac,
+                          skip_merging_rainfed_cropET=skip_merging_rainfed_cropET,
+                          skip_merging_irrigated_frac=skip_merging_irrigated_frac,
+                          skip_merging_irrigated_cropET=skip_merging_irrigated_cropET,
+                          skip_classifying_irrigated_rainfed_cropland=skip_classifying_irrigated_rainfed_cropland,
+                          skip_filtering_irrigated_rainfed_cropET=skip_filtering_irrigated_rainfed_cropET,
+                          skip_summing_irrigated_cropET=skip_summing_irrigated_cropET,
+                          skip_summing_rainfed_cropET=skip_summing_rainfed_cropET,
+                          skip_openET_processing=skip_openET_processing,
+                          skip_excess_ET_filter_processing=skip_excess_ET_filter_processing,
+                          skip_processing_slope_data=skip_processing_slope_data,
+                          skip_process_AWC_data=skip_process_AWC_data,
+                          skip_process_GrowSeason_data=skip_process_GrowSeason_data,
+                          ref_raster=WestUS_raster)
 
