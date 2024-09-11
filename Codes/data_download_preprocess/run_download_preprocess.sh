7ea2c39fa62c2ab801948b1adf58d13b2e49772f
mdfahimhasan <77580408+mdfahimhasan@users.noreply.github.com>
2024-09-11 12:40:59 -0600

    added options for job failure and time limit is sbatch script.

--- Codes/data_download_preprocess/run_download_preprocess.sh
+++ Codes/data_download_preprocess/run_download_preprocess.sh
@@ -6,7 +6,7 @@
 #SBATCH --ntasks=64
 #SBATCH --nodes=1
 #SBATCH --time=1-0
-#SBATCH --mail-type=BEGIN,END, FAIL, TIME_LIMIT
+#SBATCH --mail-type=BEGIN,END,FAIL,TIME_LIMIT
 #SBATCH --mail-user=Fahim.Hasan@colostate.edu
 
 python run_download_preprocess.py
