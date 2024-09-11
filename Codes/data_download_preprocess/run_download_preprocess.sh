c0f8444814624df72b12d36af00fc51a1f562739
mdfahimhasan <77580408+mdfahimhasan@users.noreply.github.com>
2024-09-11 11:09:51 -0600

    - job script to run scripts in HPC

---
+++ Codes/data_download_preprocess/run_download_preprocess.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+## running the run_download_preprocess.py script on CPU nodes
+
+#SBATCH --partition=smi_all
+#SBATCH --ntasks=64
+#SBATCH --nodes=1
+#SBATCH --time=1-0
+#SBATCH --mail-type=BEGIN,END
+#SBATCH --mail-user=Fahim.Hasan@colostate.edu
+
+python run_download_preprocess.py
+
